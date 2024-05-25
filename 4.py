c2c35843b3ca5aeac91b3124b42549ca5bddc0ae
souhailoukili <116470042+souhailoukili@users.noreply.github.com>
2024-05-25 17:10:43 +0200

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,9 +1,9 @@
 import telebot
-from telebot import types
-
 import requests
+import json
 import time
 
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token
 bot = telebot.TeleBot('6852601494:AAH-ejvEJirOAkatEPqTLa2rbFkmrVREW6I')
 
 # Définir l'ID du développeur
@@ -12,23 +12,49 @@ developer_id = 6631613512
 # Définir les identifiants des groupes autorisés
 allowed_group_chat_ids = [-1002136444842, -1002136444842]
 
-@bot.message_handler(commands=['SH'])
-def check_status_command(message):
-    if message.from_user.id == developer_id or message.chat.id in allowed_group_chat_ids:
-        if len(message.text.split()) == 2:
-            player_id = message.text.split()[1]
-            check_status(message, player_id)
-        else:
-            bot.reply_to(message, "𝐕𝐞𝐮𝐢𝐥𝐥𝐞𝐳 𝐟𝐨𝐮𝐫𝐧𝐢𝐫 𝐮𝐧𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐞 𝐯𝐚𝐥𝐢𝐝𝐞 {𝐩𝐚𝐫 𝐞𝐱𝐞𝐦𝐩𝐥𝐞, 𝐒𝐇 𝟏𝟐𝟑𝟒𝟓𝟔𝟕𝟖}")
-    else:
-        bot.reply_to(message, "𝐕𝐨𝐮𝐬 𝐧'ê𝐭𝐞𝐬 𝐩𝐚𝐬 𝐚𝐮𝐭𝐨𝐫𝐢𝐬é à 𝐮𝐭𝐢𝐥𝐢𝐬𝐞𝐫 𝐜𝐞𝐭𝐭𝐞 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐞 𝐝𝐚𝐧𝐬 𝐜𝐞 𝐠𝐫𝐨𝐮𝐩𝐞.")
+# Replace with your actual cookies and headers
+cookies = {
+    'source': 'mb',
+    '_gid': 'GA1.2.1236421304.1706295770',
+    '_gat_gtag_UA_137597827_4': '1',
+    'session_key': 'hnl4y8xtfe918iiz2go67z85nsrvwqdn',
+    '_ga': 'GA1.2.1006342705.1706295770',
+    'datadome': '3AmY3lp~TL1WEuDKCnlwro_WZ1C6J66V1Y0TJ4ITf1Hvo4833Fh4LF3gHrPCKFJDPUPoXh2dXQHJ_uw0ifD8jmCaDltzE5T3zzRDbXOKH9rPNrTFs29DykfP3cfo7QGy',
+    '_ga_R04L19G92K': 'GS1.1.1706295769.1.1.1706295794.0.0.0',
+}
 
-@bot.message_handler(content_types=['text'])
-def handle_text(message):
-    if message.chat.id in allowed_group_chat_ids:
-        bot.reply_to(message, "𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐭𝐨 𝐭𝐡𝐞 𝐅𝐫𝐞𝐞 𝐅𝐢𝐫𝐞 𝐁𝐚𝐧 𝐂𝐡𝐞𝐜𝐤 𝐭𝐡𝐞 𝐁𝐨𝐭! 𝐒𝐞𝐧𝐝 𝐦𝐞 𝐚 𝐩𝐥𝐚𝐲𝐞𝐫'𝐬 𝐈𝐃 𝐮𝐬𝐢𝐧𝐠  SH 𝐜𝐨𝐦𝐦𝐚𝐧𝐝 𝐭𝐨 𝐜𝐡𝐞𝐜𝐤 𝐭𝐡𝐞𝐢𝐫 𝐛𝐚𝐧 𝐬𝐭𝐚𝐭𝐮𝐬. 𝐄𝐱𝐚𝐦𝐩𝐥𝐞: SH 𝟏𝟐𝟑𝟒𝟓𝟔𝟕𝟖 🕵️‍♂️")
-    elif message.chat.type == 'private':
-        bot.reply_to(message, "𝐉𝐞 𝐧𝐞 𝐬𝐮𝐢𝐬 𝐩𝐚𝐬 𝐩𝐫𝐨𝐠𝐫𝐚𝐦𝐦é 𝐩𝐨𝐮𝐫 𝐫é𝐩𝐨𝐧𝐝𝐫𝐞 à 𝐝𝐞𝐬 𝐦𝐞𝐬𝐬𝐚𝐠𝐞𝐬 𝐩𝐫𝐢𝐯é𝐬. 𝐔𝐭𝐢𝐥𝐢𝐬𝐞𝐳-𝐦𝐨𝐢 𝐝𝐚𝐧𝐬 𝐮𝐧 𝐠𝐫𝐨𝐮𝐩𝐞 𝐨ù 𝐣𝐞 𝐬𝐮𝐢𝐬 𝐚𝐮𝐭𝐨𝐫𝐢𝐬é à 𝐟𝐨𝐧𝐜𝐭𝐢𝐨𝐧𝐧𝐞𝐫\n 𝐆𝐑𝐎𝐔𝐏 𝐋𝐈𝐍𝐂𝐊 \n 𝐡𝐭𝐭𝐩𝐬://𝐭.𝐦𝐞/𝐥𝐢𝐨𝐧𝐛𝐨𝐭_𝐯𝟏")
+headers = {
+    'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    'Connection': 'keep-alive',
+    'Origin': 'https://shop.garena.sg',
+    'Referer': 'https://shop.garena.sg/app/100067/idlogin',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36',
+    'accept': 'application/json',
+    'content-type': 'application/json',
+    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'x-datadome-clientid': 'DLm2W1ajJwdv~F~a_1d_1PyWnW6ns7GY5ChVcZY3HJ9r6D29661473aQaL2~3Nfh~Vf3m7rie7ObIb1_3eRN7J0G6uFZhMq5pM2jA828fE1dS7rZ7H3MWGQ5vGraAQWd',
+}
+
+def get_player_info_from_garena(UID):
+    json_data = {
+        'app_id': 100067,
+        'login_id': UID,
+        'app_server_id': 0,
+    }
+    
+    try:
+        response = requests.post('https://shop.garena.sg/api/auth/player_id_login', cookies=cookies, headers=headers, json=json_data)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Request failed: {e}")
+        return None
+
 def check_status(message, player_id):
     bot.send_chat_action(message.chat.id, 'typing')  # Indiquer que le bot est en train de taper
 
@@ -56,37 +82,97 @@ def check_status(message, player_id):
         result = response.json()
         is_banned = result.get('data', {}).get('is_banned', 0)
         period = result.get('data', {}).get('period', 0)
-        if is_banned == 1:
-            message_text = f"𓅓3 𝑺 𝑲 𝑹𖤍 - 𝐋𝐈𝐎𝐍 𝐒𝐎𝐔𝐇𝐀𝐈𝐋𓅓\n\n"
-            message_text += f"┏ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━\n"
-            message_text += f"┃ 𒀽   ID du joueur : {player_id}\n"
-            message_text += f"┣ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━\n"
-            message_text += f"┃ 𒀽  Statut : Banni\n"
-            message_text += f"┣ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━\n"
-            message_text += f"┃ 𒀽   Durée : {period} jours\n"
-            message_text += f"┗ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━\n"
-            message_text += f" 💻𝘿𝙚𝙫𝙚𝙡𝙤𝙥𝙚𝙧: @lion_souhail\n"
+        status_text = f"not banned" if is_banned == 0 else f"banned for {period} days"
+        
+        player_info = get_player_info_from_garena(player_id)
+        if player_info:
+            region = player_info.get('region', 'N/A')
+            nickname = player_info.get('nickname', 'N/A')
+            img_url = player_info.get('img_url', None)
+            
+            message_text = (
+                "┏━━━━━━━━━━━━━━━━━━\n"
+                "┃🧾 PLAYER INFO\n"
+                "┣━━━━━━━━━━━━━━━━━━\n"
+                f"┃🔰 ID : {player_id}\n"
+                "┣━━━━━━━━━━━━━━━━━━\n"
+                f"┃👤 NAME : {nickname}\n"
+                "┣━━━━━━━━━━━━━━━━━━\n"
+                f"┃🌐 SERVER : {region}\n"
+                "┣━━━━━━━━━━━━━━━━━━\n"
+                f"┃👾 STATUS : {status_text}\n"
+                "┣━━━━━━━━━━━━━━━━━━\n"
+                "┃💻 Développeur : @lion_souhail\n"
+                "┗━━━━━━━━━━━━━━━━━━"
+            )
+            if img_url:
+                bot.send_photo(message.chat.id, img_url, caption=message_text)
+            else:
+                bot.reply_to(message, message_text)
         else:
-            message_text = f"𓅓3 𝑺 𝑲 𝑹𖤍 - 𝐋𝐈𝐎𝐍 𝐒𝐎𝐔𝐇𝐀𝐈𝐋𓅓\n\n"
-            message_text += f"┏ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━\n"
-            message_text += f"┃ 𒀽   ID du joueur : {player_id}\n"
-            message_text += f"┣ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━\n"
-            message_text += f"┃ 𒀽  Statut : Non Banni\n"
-            message_text += f"┗ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━\n"
-            message_text += f" 💻𝘿𝙚𝙫𝙚𝙡𝙤𝙥𝙚𝙧: @lion_souhail\n"
-
-        # Construire le clavier en ligne
-        keyboard = types.InlineKeyboardMarkup(row_width=1)
-        url_button = types.InlineKeyboardButton(text="𝐕𝐨𝐢𝐫 𝐈𝐧𝐬𝐭𝐚𝐠𝐫𝐚𝐦 ❤️", url="https://www.instagram.com/blrx__souhail?igsh=bXhwd2FuMXd2cXh4")
-        keyboard.add(url_button)
-
-        # Envoyer la réponse avec le clavier en ligne
-        sent_message = bot.send_message(message.chat.id, message_text, reply_markup=keyboard)
-
+            message_text = (
+                "┏━━━━━━━━━━━━━━━━━━\n"
+                "┃🧾 PLAYER INFO\n"
+                "┣━━━━━━━━━━━━━━━━━━\n"
+                f"┃🔰 ID : {player_id}\n"
+                "┣━━━━━━━━━━━━━━━━━━\n"
+                f"┃👾 STATUS : {status_text}\n"
+                "┣━━━━━━━━━━━━━━━━━━\n"
+                "┃💻 Développeur : @lion_souhail\n"
+                "┗━━━━━━━━━━━━━━━━━━"
+            )
+            bot.reply_to(message, message_text)
+        
         # Supprimer le message "recherche d'informations" après un délai (par exemple, 3 secondes)
         time.sleep(3)
         bot.delete_message(message.chat.id, searching_message.message_id)
     else:
-        bot.reply_to(message, "𝐈𝐦𝐩𝐨𝐬𝐬𝐢𝐛𝐥𝐞 𝐝𝐞 𝐫é𝐜𝐮𝐩é𝐫𝐞𝐫 𝐥𝐞𝐬 𝐝𝐨𝐧𝐧é𝐞𝐬 𝐝𝐞𝐩𝐮𝐢𝐬 𝐥'𝐀𝐏𝐈")
+        bot.reply_to(message, "Impossible de récupérer les données depuis l'API")
+
+@bot.message_handler(commands=['SH'])
+def check_status_command(message):
+    if message.from_user.id == developer_id or message.chat.id in allowed_group_chat_ids:
+        if len(message.text.split()) == 2:
+            player_id = message.text.split()[1]
+            check_status(message, player_id)
+        else:
+            bot.reply_to(message, "Usage: /SH <player_id>")
+    else:
+        bot.reply_to(message, "Vous n'êtes pas autorisé à utiliser cette commande.")
+
+@bot.message_handler(func=lambda message: True)
+def get_player_info(message):
+    if message.chat.id in allowed_group_chat_ids or message.from_user.id == developer_id:
+        try:
+            UID = message.text.strip()
+            player_info = get_player_info_from_garena(UID)
+            if player_info:
+                region = player_info.get('region', 'N/A')
+                nickname = player_info.get('nickname', 'N/A')
+                img_url = player_info.get('img_url', None)
+                
+                message_text = (
+                    "┏━━━━━━━━━━━━━━━━━━\n"
+                    "┃🧾 PLAYER INFO\n"
+                    "┣━━━━━━━━━━━━━━━━━━\n"
+                    f"┃🔰 ID : {UID}\n"
+                    "┣━━━━━━━━━━━━━━━━━━\n"
+                    f"┃👤 NAME : {nickname}\n"
+                    "┣━━━━━━━━━━━━━━━━━━\n"
+                    f"┃🌐 SERVER : {region}\n"
+                    "┣━━━━━━━━━━━━━━━━━━\n"
+                    "┃💻 Développeur : @lion_souhail\n"
+                    "┗━━━━━━━━━━━━━━━━━━"
+                )
+                if img_url:
+                    bot.send_photo(message.chat.id, img_url, caption=message_text)
+                else:
+                    bot.reply_to(message, message_text)
+            else:
+                bot.reply_to(message, "Les informations du joueur ne sont pas disponibles.")
+        except Exception as e:
+            bot.reply_to(message, f"Une erreur est survenue : {str(e)}")
+    else:
+        bot.reply_to(message, "Je ne suis pas programmé pour répondre dans ce groupe. Contactez le développeur pour plus d'informations.")
 
 bot.polling()
